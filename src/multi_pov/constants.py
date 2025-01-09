a36437c83d29296dea8f762fcdc20e98bd51be56
hemakodaa <hemakodaa@gmail.com>
2025-01-09 15:29:11 +0700

    added offset and dl folder to repo

--- src/multi_pov/constants.py
+++ src/multi_pov/constants.py
@@ -1,5 +1,5 @@
 YT_DLP = "yt-dlp"
-DOWNLOAD_FOLDER = "downloads"
+DOWNLOAD_FOLDER = "dl"
 OFFSET_FOLDER = "offset"
 DEFAULT_RESOLUTION = 1080
 FILENAME = "%(section_start)s_%(section_end)s_%(title)s.%(ext)s"
